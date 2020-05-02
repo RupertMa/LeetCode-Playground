91117d79ce771f305fcc0dbc452a311ab630621a
yibo-ma2 <yibo_ma@apple.com>
2020-05-01 21:24:27 -0700

    More concise code

--- 23.Merge k Sorted Lists.py
+++ 23.Merge k Sorted Lists.py
@@ -1,28 +1,24 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 ListNode.__lt__ = lambda x, y : x.val < y.val
+
 
 class Solution:
     import heapq
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         dummy = ListNode(None)
         curNode = dummy
-        heap = []
-        
-        for lst in lists:
-            if lst:
-                heapq.heappush(heap, lst)
+        lists = [l for l in lists if l]
+        heapq.heapify(lists)
                 
-        while heap:
-            head = heapq.heappop(heap)
-            curNode.next = head
-            curNode = head
-            if head.next:
-                heapq.heappush(heap, head.next)
+        while lists:
+            curNode.next = heapq.heappop(lists)
+            curNode = curNode.next
+            if curNode.next:
+                heapq.heappush(lists, curNode.next)
                 
         return dummy.next
-        
