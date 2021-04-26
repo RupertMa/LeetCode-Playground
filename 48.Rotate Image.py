bd23d14089f7c26e91c444f0870e63de4d0345e7
Yibo Ma <yiboma@usc.edu>
2021-04-25 22:21:53 -0700

    New commit

--- 48.Rotate Image.py
+++ 48.Rotate Image.py
@@ -7,10 +7,10 @@ class Solution:
         for i in range(n//2):
             for j in range(i, n-i-1):
                 temp = matrix[i][j]
-                matrix[i][j] = matrix[n-i-1][i]
-                matrix[n-i-1][j] = matrix[n-j-1][n-i-1]
-                matrix[n-j-1][n-i-1] = matrix[i][n-j-1]
-                matrix[i][n-j-1] = temp
+                matrix[i][j] = matrix[n-j-1][i]
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1]
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1]
+                matrix[j][n-i-1] = temp
 
-x = Solution()
-print(x.rotate([[1,2,3],[4,5,6],[7,8,9]]))
+       # Time complexity: O(N^2)
+       # Space complexity: O(1)
