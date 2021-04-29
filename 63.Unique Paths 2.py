de270ceae341b9405d639a29c02df436cfc88055
Yibo Ma <yiboma@usc.edu>
2021-04-28 23:15:02 -0700

    New commit

--- 63.Unique Paths 2.py
+++ 63.Unique Paths 2.py
@@ -23,3 +23,21 @@ class Solution:
     # Space complexity: O(N*M)
 
     # Actually, space complexity can be optimized to O(N)
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m = len(obstacleGrid) ; n = len(obstacleGrid[0])
+        dp = [[0]*n for i in range(2)]
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j]:
+                    dp[i%2][j] = 0
+                    continue
+
+                if i !=0 and j!=0:
+                    dp[i%2][j] = dp[(i-1)%2][j] + dp[i%2][j-1]
+                elif i==0 and j!=0:
+                    dp[i%2][j] = dp[i%2][j-1]
+                elif i!=0 and j==0:
+                    dp[i%2][j] = dp[(i-1)%2][j]
+                else:
+                    dp[i%2][j] = 1
+        return dp[i%2][j]
