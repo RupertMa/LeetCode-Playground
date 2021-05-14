d286f2bf308377a1953c7f01c97ff2f57b8da454
Yibo Ma <yiboma@usc.edu>
2021-05-13 22:09:41 -0700

    New commit

--- 49.Group Anagrams.py
+++ 49.Group Anagrams.py
@@ -8,4 +8,17 @@ class Solution:
             ans[tuple(sorted(s))].append(s)
         return ans.values()
 
-        # Time complexity: O(Nlog(M)) N = len(strs), M = max(len(s) for s in str)
+        # Time complexity: O(NMlog(M)) N = len(strs), M = max(len(s) for s in str)
+        # Space complexity: O(N)
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        from collections import defaultdict
+        ans = defaultdict(list)
+        for s in strs:
+            key = [0] * 26
+            for i in s:
+                key[ord(i)-ord('a')] += 1
+            ans[tuple(key)].append(s)
+        return ans.values()
+        # Time complexity: O(NM) N = len(strs), M = max(len(s) for s in str)
+        # Space complexity: O(N)
