7f28355a5d45daefc189d8e08368e2f20706484d
Yibo Ma <yiboma@usc.edu>
2021-06-28 21:55:22 -0700

    New commit

--- 338.Counting Bits.py
+++ 338.Counting Bits.py
@@ -12,11 +12,20 @@ class Solution(object):
         return b
 
 
-
-
-
-num=3
-ans = [0]
-for x in range(1, num + 1):
-	ans += ans[x >> 1] + (x & 1),
-print ans
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        if n == 0:
+            return [0]
+        elif n == 1:
+            return [0, 1]
+        ans = [0, 1]
+        length = 2
+        pointer = 0
+        while length <= n:
+            prev_len = len(ans)
+            while pointer < prev_len and length <= n:
+                ans.append(ans[pointer]+1)
+                pointer += 1
+                length += 1
+            pointer = 0
+        return ans
