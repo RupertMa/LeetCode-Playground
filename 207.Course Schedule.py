1fd885baaf87508648008d903090c882c7d99b22
Yibo Ma <yiboma@usc.edu>
2021-07-31 11:44:28 -0700

    New commit

--- 207.Course Schedule.py
+++ 207.Course Schedule.py
@@ -1,3 +1,5 @@
+# Kahn's algorithm
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         from collections import defaultdict
@@ -20,3 +22,47 @@ class Solution:
             return False
         else:
             return True
+
+
+# DFS
+class Solution:
+    def __init__(self):
+        self.l = []
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        global edges
+        global unmarked
+        global temp
+        global permanent
+
+        from collections import defaultdict
+        edges = defaultdict(list)
+        for (a, b) in prerequisites:
+            edges[b].append(a)
+
+
+        unmarked = set([i for i in range(numCourses)])
+        temp = set()
+        permanent = set()
+        while len(permanent) != numCourses:
+            n = unmarked.pop()
+            if not self.visit(n):
+                return False
+        return True
+
+    def visit(self, n):
+        if n in permanent:
+            return True
+        if n in temp:
+            print('should be here')
+            return False
+
+        temp.add(n)
+        for m in edges[n]:
+            if not self.visit(m):
+                return False
+        temp.remove(n)
+        unmarked.discard(n)
+        permanent.add(n)
+        self.l = [n] + self.l
+        return True
