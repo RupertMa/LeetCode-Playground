5f105b695b5a46d3098dac5ca0f3591f487aa618
yiboma <yiboma@usc.edu>
2025-02-28 20:55:39 -0800

    2025-02-28

--- 1700.Number of Students Unable to Eat Lunch.py
+++ 1700.Number of Students Unable to Eat Lunch.py
@@ -16,3 +16,24 @@ class Solution:
 
         # Time complexity: O(N**2)
         # Space complexity: O(1)
+
+# 2025/02/28
+# Time complexity: O(N**2)
+# Space complexity: O(1)
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        all_students = False
+        while not all_students and sandwiches:
+            cur = sandwiches[0]
+            num_students = len(students)
+            idx = 0
+            while idx < num_students:
+                if students[idx] == cur:
+                    students = students[idx + 1:] + students[: idx]
+                    sandwiches.pop(0)
+                    break
+                else:
+                    idx += 1
+            all_students = (idx >= num_students)
+
+        return len(sandwiches)
