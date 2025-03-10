e7a9704265d151ccc7929a805f8acaff17b943e1
yiboma <yiboma@usc.edu>
2025-03-09 20:57:29 -0700

    2025-03-09

--- 394.Decode String.py
+++ 394.Decode String.py
@@ -21,3 +21,44 @@ class Solution:
 
         # Time complexity: O(N)
         # Space complexity: O(N)
+
+
+# 2025/03/09
+# Time complexity: O(N)
+# Space complexity: O(N)
+import string
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        idx = 0
+        int_str = {str(i) for i in range(10)}
+        letter_set = set(string.ascii_lowercase)
+        while idx < len(s):
+            cur = s[idx]
+            idx += 1
+            if cur != ']':
+                stack.append(cur)
+            else:
+                str_part = ''
+                digit_part = ''
+                cond = False
+                while stack:
+                    x = stack.pop()
+                    if not cond and x in letter_set:
+                        str_part = x + str_part
+                    elif not cond and x == '[':
+                        cond = True
+                        continue
+                    elif x in int_str:
+                        digit_part = x + digit_part
+                    else:
+                        pass
+
+                    if (cond and x not in int_str) or (not stack):
+                        if stack:
+                            stack.append(x)
+                        if not digit_part:
+                            digit_part = '1'
+                        stack.extend(list(int(digit_part) * str_part))
+                        break
+        return ''.join(stack)
